db22fc8b34b4ee70673c173b12d268b8ae195c8f
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-04-07 19:25:59 +0500

    changes

--- twinkabakers/urls.py
+++ twinkabakers/urls.py
@@ -19,6 +19,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='imdex'),
+    path('', views.index, name='index'),
+    path('cakes', views.cakes, name='cakes'),
 
 ]
